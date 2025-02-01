3afd6f8ebb92e052dd19f60225b0c0e1aec4074f
Lucas Marcolongo <lucas_marco@live.com>
2025-01-31 16:56:46 -0800

    feat(frontend): remove NxWelcomeComponent and update app component structure

--- apps/frontend/src/app/app.component.stories.ts
+++ apps/frontend/src/app/app.component.stories.ts
@@ -1,7 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { AppComponent } from './app.component';
-import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
 
 const meta: Meta<AppComponent> = {
   component: AppComponent,
@@ -12,12 +10,4 @@ type Story = StoryObj<AppComponent>;
 
 export const Primary: Story = {
   args: {},
-};
-
-export const Heading: Story = {
-  args: {},
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/app works!/gi)).toBeTruthy();
-  },
 };
