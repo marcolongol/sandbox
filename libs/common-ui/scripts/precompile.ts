48159a4878d0f0626f86242905e00cf71985a2c3
Lucas Marcolongo <lucas_marco@live.com>
2025-02-03 15:45:37 -0800

    feat(common-ui): add precompile dependency and streamline Tailwind CSS processing

--- libs/common-ui/scripts/precompile.ts
+++ libs/common-ui/scripts/precompile.ts
@@ -1,5 +1,3 @@
-import { readFile, writeFile } from 'fs';
-
 // eslint-disable-next-line @nx/enforce-module-boundaries
 import { sync } from 'glob';
 
@@ -7,15 +5,12 @@ import { exec } from 'child_process';
 
 const libRoot = '../';
 const tailwindConf = 'tailwind.config.ts';
-const processedExt = '.precompiled.scss';
-const styleRegex = /styleUrls:\s*\[([^\]]+)]/;
 
 // Find all `.scss` files and tailwind process them
 sync(`${libRoot}/**/*.scss`).forEach((file) => {
-  const cssFile = file.replace(/\.scss$/, processedExt);
-  console.log('🚀 ~ sync ~ cssFile:', cssFile);
+  console.log('🚀 ~ sync ~ cssFile:', file);
   exec(
-    `npx tailwind -c ${tailwindConf} -i ${file} -o ${cssFile}`,
+    `npx tailwind -c ${tailwindConf} -i ${file} -o ${file}`,
     (err, stdout, stderr) => {
       if (err) {
         console.error(stderr);
@@ -23,27 +18,4 @@ sync(`${libRoot}/**/*.scss`).forEach((file) => {
       }
     }
   );
-});
-
-// Find all components with `styleUrls` and switch `.scss` extension to our precompiled file names
-sync(`${libRoot}/**/*.component.ts`).forEach((file) => {
-  readFile(file, (err, data) => {
-    if (err) throw err;
-    const content = data.toString();
-    const match = content.match(styleRegex);
-    if (match) {
-      const styleUrls = match[1]
-        .split(',')
-        .map((s) => s.trim().replace('.scss', processedExt))
-        .join(', ');
-
-      writeFile(
-        file,
-        content.replace(styleRegex, `styleUrls: [${styleUrls}]`),
-        (err) => {
-          if (err) throw err;
-        }
-      );
-    }
-  });
 });
