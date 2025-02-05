0ab5a6a7363fbbd0fef12f70fad57412577b5a4d
Lucas Marcolongo <lucas_marco@live.com>
2025-02-05 02:34:20 -0800

    fix(e2e): update Playwright config for local server and modify test case

--- apps/frontend-e2e/src/example.spec.ts
+++ apps/frontend-e2e/src/example.spec.ts
@@ -1,8 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('basic test', async ({ page }) => {
   await page.goto('/');
-
-  // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Welcome');
+  const homeWorks = await page.$('text=home works!');
+  expect(homeWorks).toBeTruthy();
 });
